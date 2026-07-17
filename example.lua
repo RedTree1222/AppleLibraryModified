cd05a786c7beb457bd9654bd93a4cbe3e7d4cf39
RedTree1222 <106989008+RedTree1222@users.noreply.github.com>
2026-07-17 13:21:14 +0200

    change example name

--- example.lua
+++ example.lua
@@ -1,6 +1,6 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RedTree1222/AppleLibraryModified/refs/heads/main/main.lua"))()
 
-library.FolderName = "Eclipsed"
+library.FolderName = "Example"
 
 local window = library:init("Titlebar", true, Enum.KeyCode.LeftControl, true)
 
